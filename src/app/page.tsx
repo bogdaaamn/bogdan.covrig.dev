773359db4b2d6f10bf6d2f63bd5116c6e9d75ae9
Bogdan Covrig <bogcov@gmail.com>
2024-09-13 14:39:59 +0200

    feat: Add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
 export default function Page() {
   return (
-    <main>
-      <h1>Hello Bogdan</h1>
+    <main className="flex justify-center items-center h-screen p-4 pb-20">
+      <div className="[&>*]:mb-2 max-w-[600px] text-sm text-balance">
+        <h1>bogdan covrig</h1>
+        <p className="mt-4">
+          software engineer in the netherlands. into dev tools, privacy, and automation. cool with typescript, react,
+          and node. currently building e-commerce platforms
+        </p>
+        <p>usually busy with movies, concerts, and sitting in the park for no apparent reasons. oxford comma enjoyer</p>
+        <p className="mt-4">
+          <a href="https://read.cv/bogdaaamn" target="_blank" rel="noopener noreferrer">
+            read.cv/bogdaaamn
+          </a>
+        </p>
+        <p>
+          <a href="https://github.com/bogdaaamn" target="_blank" rel="noopener noreferrer">
+            github.com/bogdaaamn
+          </a>
+        </p>
+        <p>
+          <a href="https://twitter.com/bogdancovrig" target="_blank" rel="noopener noreferrer">
+            twitter.com/bogdancovrig
+          </a>
+        </p>
+      </div>
     </main>
   );
 }
