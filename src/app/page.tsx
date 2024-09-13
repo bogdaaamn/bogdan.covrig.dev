432dd388990594a2277f23d4a003995240e6a523
Bogdan Covrig <bogcov@gmail.com>
2024-09-13 15:46:38 +0200

    feat: Add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,44 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://bogdan.covrig.dev"),
+  alternates: {
+    canonical: "/",
+  },
+  title: "index.html / bogdan covrig",
+  description:
+    "software engineer in the netherlands. into dev tools, privacy, and automation. cool with typescript, react,and node. currently building e-commerce platforms",
+  openGraph: {
+    title: "index.html / bogdan covrig",
+    description:
+      "software engineer in the netherlands. into dev tools, privacy, and automation. cool with typescript, react,and node. currently building e-commerce platforms",
+    url: "https://bogdan.covrig.dev",
+    siteName: "index.html / bogdan covrig",
+    images: [
+      {
+        url: "https://bogdan.covrig.dev/og.png",
+        width: 1200,
+        height: 630,
+        alt: "index.html / bogdan covrig",
+      },
+    ],
+    locale: "en",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "index.html / bogdan covrig",
+    description:
+      "software engineer in the netherlands. into dev tools, privacy, and automation. cool with typescript, react,and node. currently building e-commerce platforms",
+    creator: "@BogdanCovrig",
+    images: ["https://bogdan.covrig.dev/og.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
+
 export default function Page() {
   return (
     <main className="flex justify-center items-center h-screen p-4 pb-20">
