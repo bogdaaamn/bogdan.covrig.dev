8e0c63778cd88c8fbe5cdf9b7c3124041f983332
Bogdan Covrig <bogcov@gmail.com>
2025-05-22 15:51:11 +0200

    chore: Update Tailwind CSS to version 4.1.7, add @tailwindcss/postcss, and refactor PostCSS configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="flex justify-center items-center h-screen p-4 pb-20">
-      <div className="[&>*]:mb-2 max-w-[600px] text-sm text-balance">
+      <div className="*:mb-2 max-w-[600px] text-sm text-balance">
         <h1>bogdan covrig</h1>
         <p className="mt-4">
           software engineer in the netherlands. into dev tools, privacy, and automation. cool with typescript, react,
