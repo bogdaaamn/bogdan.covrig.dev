0e04b9986aa2aa8db01534b96a04db8edcbb662b
Bogdan Covrig <bogcov@gmail.com>
2025-09-11 14:53:09 +0200

    feat: Update personal description and replace GitHub/Twitter links with new bsky profile link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,23 +45,23 @@ export default function Page() {
       <div className="*:mb-2 max-w-[600px] text-sm text-balance">
         <h1>bogdan covrig</h1>
         <p className="mt-4">
-          software engineer in the netherlands. into dev tools, privacy, and automation. cool with typescript, react,
+          software engineer in the netherlands. dev tools, privacy, and automation. typescript, next,
           and node. currently building e-commerce platforms
         </p>
         <p>usually busy with movies, concerts, and sitting in the park for no apparent reasons. oxford comma enjoyer</p>
+        <p>
+          <a href="https://www.linkedin.com/in/bogdancovrig/" target="_blank" rel="noopener noreferrer">
+            linkedin.com/in/bogdancovrig
+          </a>
+        </p>
         <p className="mt-4">
           <a href="https://read.cv/bogdaaamn" target="_blank" rel="noopener noreferrer">
             read.cv/bogdaaamn
           </a>
         </p>
         <p>
-          <a href="https://github.com/bogdaaamn" target="_blank" rel="noopener noreferrer">
-            github.com/bogdaaamn
-          </a>
-        </p>
-        <p>
-          <a href="https://twitter.com/bogdancovrig" target="_blank" rel="noopener noreferrer">
-            twitter.com/bogdancovrig
+          <a href="https://bsky.app/profile/bogdan.covrig.dev" target="_blank" rel="noopener noreferrer">
+            bsky.app/profile/bogdan.covrig.dev
           </a>
         </p>
       </div>
